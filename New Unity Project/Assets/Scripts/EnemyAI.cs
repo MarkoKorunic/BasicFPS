333ef8ba4a6ae5bf653a2f86b0697ff8673727c7
Marko Korunić <marko.korunic29@gmail.com>
2021-11-02 13:15:08 +0100

    Created more advanced AI
    More advanced AI that chases player. Also added attack method, some bools and created better architecture for future logic and game features.

--- New Unity Project/Assets/Scripts/EnemyAI.cs
+++ New Unity Project/Assets/Scripts/EnemyAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
-
+using System;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -11,6 +11,7 @@ public class EnemyAI : MonoBehaviour
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     private void Start()
     {
@@ -20,10 +21,44 @@ public class EnemyAI : MonoBehaviour
     private void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if(distanceToTarget <= chaseRange)
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
         }
+
+        else if(distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("Enemy attacks player.");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 
 }
