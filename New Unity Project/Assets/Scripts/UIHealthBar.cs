6ddea558a363e738a576f02971792c5821de46d2
Marko Korunić <marko.korunic29@gmail.com>
2021-11-11 18:03:20 +0100

    Health bar now change color according to health value. Also deleted some unneccesary materials.

--- New Unity Project/Assets/Scripts/UIHealthBar.cs
+++ New Unity Project/Assets/Scripts/UIHealthBar.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 
 public class UIHealthBar : MonoBehaviour
 {
-    [SerializeField]PlayerHealth playerHealth;
+    [SerializeField] PlayerHealth playerHealth;
     public Slider slider;
-
+    public Image healthColor;
 
 
     private void Update()
     {
         SetPlayerHealth();
+       
     }
     public void SetPlayerHealth()
     {
         slider.value = playerHealth.currentHitPoints;
+        HealthBarColorChange();
     }
 
+    public void HealthBarColorChange()
+    {
+        if (slider.value > 500f)
+            healthColor.color = Color.green;
+        if (slider.value <= 500f && slider.value > 200)
+            healthColor.color = Color.yellow;
+        if (slider.value <= 200)
+            healthColor.color = Color.red;
+    }
 }
