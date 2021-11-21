21eca8f7a9a5e1c91a651503aae76baaa194b2f9
Marko Korunić <marko.korunic29@gmail.com>
2021-11-21 18:37:21 +0100

    Health handler now handles enemy and player health.

--- New Unity Project/Assets/Scripts/Enemy.cs
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -12,49 +12,42 @@ public class Enemy : MonoBehaviour
     [SerializeField] public AudioClip enemyDeathSound;
     [SerializeField] AudioSource source;
 
-    public float damage = 0f;
-    public float health;
+
+    public HealthHandler healthHandler;
+    public float initialHealth;
 
     private float size;
 
     private void OnEnable()
     {
         SetEnemyAttributes();
-
+        healthHandler = new HealthHandler(this.OnHealthChange, this.initialHealth);
         source = GetComponent<AudioSource>();
     }
 
     public void TakeDamage(float damageAmount)
     {
-        health -= damageAmount;
-        if (health <= 0f)
-        {
-            EnemyDeath();
-        }
-        if (health > 0f)
-        {
-            Debug.Log("Enemy taken " + damageAmount + " damage!");
-        }
+        healthHandler.TakeDamage(damageAmount);
     }
     
     void SetEnemySize(float size)
     {
         gameObject.transform.localScale += new Vector3(size,size,size);
     }
-    private void SetEnemyAttributes()
-    {
-        EnemySO enemySO = GetRandomEnemySO();
-        size = (float)enemySO.enemySize;
-        SetEnemySize(size);
-        health = enemySO.health;
-    }
+   
 
     public EnemySO GetRandomEnemySO()
     {
         return enemies[UnityEngine.Random.Range(0, enemies.Length - 1)];
     }
 
-    void EnemyDeath()
+    private void OnHealthChange(float health)
+    {
+        if (health <= 0)
+            EnemyDeath();
+    }
+
+    private void EnemyDeath()
     {
         enemyModel.animator.SetBool("Death", true);
         source.PlayOneShot(enemyDeathSound);
@@ -62,4 +55,12 @@ public class Enemy : MonoBehaviour
         Debug.Log("Enemy died!!");
     }
 
+
+    private void SetEnemyAttributes()
+    {
+        EnemySO enemySO = GetRandomEnemySO();
+        size = (float)enemySO.enemySize;
+        SetEnemySize(size);
+        initialHealth = enemySO.health;
+    }
 }
