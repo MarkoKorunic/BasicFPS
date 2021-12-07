eb0b4892e867c9a354e0e33bd1575451780de4ce
Marko Korunić <marko.korunic29@gmail.com>
2021-12-07 13:10:06 +0100

    Main menu now loads into main game scene + more features.

--- New Unity Project/Assets/Scripts/MainMenu/MenuController.cs
+++ New Unity Project/Assets/Scripts/MainMenu/MenuController.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Levels to load")]
+    [SerializeField] private GameObject noSavedGameDialogue = null;
+    public string newGameLoad;
+    private string levelToLoad;
+
+
+
+    public void NewGameDialogueYes()
     {
-        
+        SceneManager.LoadScene(newGameLoad);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGameDialogueYes()
     {
-        
+        if (PlayerPrefs.HasKey("SavedGame"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedGame");
+            SceneManager.LoadScene(levelToLoad);
+        }
+
+        else
+        {
+            noSavedGameDialogue.SetActive(true);
+        }
+    }
+
+
+    public void QuitGameButton()
+    {
+        Application.Quit();
     }
 }
