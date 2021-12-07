016cdd1ccfaa2418a66bebe02c1637a9c1929d4b
Marko Korunić <marko.korunic29@gmail.com>
2021-12-07 14:38:01 +0100

    added sound submenu

--- New Unity Project/Assets/Scripts/MainMenu/MenuController.cs
+++ New Unity Project/Assets/Scripts/MainMenu/MenuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
@@ -10,6 +11,14 @@ public class MenuController : MonoBehaviour
     [SerializeField] private GameObject noSavedGameDialogue = null;
     public string newGameLoad;
     private string levelToLoad;
+
+    [Header("Confirmation Prompt")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+    [Header("Volume setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 0.5f;
 
 
 
@@ -37,4 +46,31 @@ public class MenuController : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void VolumeReset()
+    {
+        AudioListener.volume = defaultVolume;
+        volumeSlider.value = defaultVolume;
+        volumeTextValue.text = defaultVolume.ToString("0.0");
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(1);
+        confirmationPrompt.SetActive(false);
+    }
 }
+
